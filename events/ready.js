34f4c08e4c523f8a76720868c6e3d375526e1f41
DanteIn4k <82333197+CodeMaxxer69@users.noreply.github.com>
2024-07-13 14:04:04 +0530

    Add files via upload

--- events/ready.js
+++ events/ready.js
@@ -21,35 +21,40 @@
 
 
 const config = require("../config.js");
-const { ActivityType  } = require("discord.js")
+const { ActivityType } = require("discord.js");
+
 module.exports = async (client) => {
+    const { REST } = require("@discordjs/rest");
+    const { Routes } = require("discord-api-types/v10");
+    const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
 
+    (async () => {
+        try {
+            await rest.put(Routes.applicationCommands(client.user.id), {
+                body: await client.commands,
+            });
+            console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!');
+        } catch (err) {
+            console.log('\x1b[36m%s\x1b[0m', '|    ❌ Commands Failed To Load!');
+        }
+    })();
 
+    console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
+    const activityType = ActivityType[config.activityType.charAt(0).toUpperCase() + config.activityType.slice(1).toLowerCase()];
+    if (!activityType) {
+        console.error(`Invalid activity type: ${config.activityType}`);
+        return;
+    }
+    
+    setInterval(() => client.user.setActivity({ 
+        name: config.activityName, 
+        type: activityType 
+    }), 10000);
 
-const { REST } = require("@discordjs/rest");
-const { Routes } = require("discord-api-types/v10");
-const rest = new REST({ version: "10" }).setToken(config.TOKEN || process.env.TOKEN);
-(async () => {
-try {
-await rest.put(Routes.applicationCommands(client.user.id), {
-body: await client.commands,
-});
-console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Loaded successfully!')
-} catch (err) {
-console.log('\x1b[36m%s\x1b[0m', '|    ❌ Commands Failed To Load!');
-}
-})();
+    client.errorLog = config.errorLog;
+};
 
-console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
-
-const serverCount = client.guilds.cache.size;
-setInterval(() => client.user.setActivity({ 
-  name:`lavaLink GlaceYT`, 
-  type: ActivityType.Listening }), 10000);
-client.errorLog = config.errorLog
-  
-}
 
 /*
 
