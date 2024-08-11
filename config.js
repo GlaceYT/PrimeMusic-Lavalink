54f5b2bda2bbd9127e4b8138af0817e84e4fcf9c
fifa00722 <132184628+fifa00722@users.noreply.github.com>
2024-08-11 23:41:35 +0700

    Update config.js

--- config.js
+++ config.js
@@ -40,13 +40,10 @@ module.exports = {
 
   nodes: [
     {
-        name: "Node #1",
         host: "lava-v3.ajieblogs.eu.org",
-        port: 80,
+        port: 443,
         password: "https://dsc.gg/ajidevserver",
-        reconnectTimeout: 5000,
-        reconnectTries: Infinity,
-        secure: false
+        secure: true
     },
  ]
 }
