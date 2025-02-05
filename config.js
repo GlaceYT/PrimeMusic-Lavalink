af676eddc7c100a75ec8baabc37411816ff08f30
Appuffyt <lisaexe6@gmail.com>
2025-02-05 13:17:38 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,14 +3,14 @@
 module.exports = {
   TOKEN: "",
   language: "en",
-  ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  ownerID: ["1289254881898663988", ""], 
+  mongodbUri : "mongodb+srv://codex-in1:codex-in1@codex-in1.1bgxp.mongodb.net/?retryWrites=true&w=majority",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
   activityName: "YouTube Music", 
   activityType: "LISTENING",  // Available activity types : LISTENING , PLAYING
-  SupportServer: "https://discord.gg/xQF9f9yUEM",
+  SupportServer: "https://discord.gg/hAebvqCU",
   embedTimeout: 5, 
   errorLog: "", 
   nodes: [
