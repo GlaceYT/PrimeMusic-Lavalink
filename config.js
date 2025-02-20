d35e1bff3b5a03ef95209a4c83f61d98d4794544
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2025-02-20 15:50:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,10 +15,10 @@ module.exports = {
   errorLog: "", 
   nodes: [
      {
-      name: "Koi Node V4",
-      password: "saher.inzeworld.com",
-      host: "lava.inzeworld.com",
-      port: 3128,
+      name: "XX",
+      password: "enteryourcustompass",
+      host: "180.188.226.76",
+      port:  7019,
       secure: false
     }
   ]
