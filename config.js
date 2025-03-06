1e5408997401de9b29f6646add5e10eb1d1603b2
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2025-03-06 13:21:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,8 @@ module.exports = {
   language: "en",
   ownerID: ["1004206704994566164", ""], 
   mongodbUri : "",
+  spotifyClientId : "f71a3da30e254962965ca2a89d6f74b9",
+  spotifyClientSecret : "199a619d22dd4e55a4a2c1a7a3d70e63",
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "#1db954",
