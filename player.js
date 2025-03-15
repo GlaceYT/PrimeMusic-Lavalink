2d271da362edb0daafda5f245c4ed99ea5cc25f9
Appuffyt <lisaexe6@gmail.com>
2025-03-15 17:13:02 +0530

    Update player.js

--- player.js
+++ player.js
@@ -102,7 +102,7 @@ function initializePlayer(client) {
                 iconURL: musicIcons.playerIcon,
                 url: config.SupportServer
             })
-            .setFooter({ text: `Developed by SSRR | Prime Music v1.2`, iconURL: musicIcons.heartIcon })
+            .setFooter({ text: `Developed by Nettu | Netturan`, iconURL: musicIcons.heartIcon })
             .setTimestamp()
             .setDescription(  
                 `- **Title:** [${track.info.title}](${track.info.uri})\n` +
