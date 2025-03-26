2ba8c30150d416130bfa1d95e4bb530a79506ac9
GuangyuZhu <144188710+Andy-gyz@users.noreply.github.com>
2025-03-26 22:45:23 +1100

    Update config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb + srv://shiva:shiva@mucisbotyt.ouljywv.mongodb.net/?retryWrities=true&w=majcrity",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
