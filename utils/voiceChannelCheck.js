309d32b14ecfeff1494610fe56c05336c62ca69c
GlaceYT <96680732+GlaceYT@users.noreply.github.com>
2025-12-06 18:32:55 +0530

    Update voiceChannelCheck.js

--- utils/voiceChannelCheck.js
+++ utils/voiceChannelCheck.js
@@ -2,16 +2,35 @@ const { ContainerBuilder, MessageFlags } = require('discord.js');
 const { getLang } = require('./languageLoader.js');
 
 async function checkVoiceChannel(interaction, player) {
-    const lang = getLang(interaction.guildId);
+
+    const lang = await getLang(interaction.guildId).catch(() => {
+     
+        return require('../languages/en.js');
+    });
+    
+
+    const utils = lang?.utils || {};
+    const voiceCheck = utils?.voiceChannelCheck || {
+        noVoiceChannel: {
+            title: "## ❌ No Voice Channel",
+            message: "You need to be in a voice channel to use this command.",
+            note: "Please join a voice channel and try again."
+        },
+        wrongChannel: {
+            title: "## 🎵 Join Voice Channel",
+            message: "The bot is currently active in **{channelName}**.",
+            note: "Please join **{channelName}** to use music commands."
+        }
+    };
     
     if (!interaction.member.voice.channelId) {
         const errorContainer = new ContainerBuilder()
             .setAccentColor(0xff0000)
             .addTextDisplayComponents(
                 (textDisplay) => textDisplay.setContent(
-                    `${lang.utils.voiceChannelCheck.noVoiceChannel.title}\n\n` +
-                    `${lang.utils.voiceChannelCheck.noVoiceChannel.message}\n` +
-                    `${lang.utils.voiceChannelCheck.noVoiceChannel.note}`
+                    `${voiceCheck.noVoiceChannel.title}\n\n` +
+                    `${voiceCheck.noVoiceChannel.message}\n` +
+                    `${voiceCheck.noVoiceChannel.note}`
                 )
             );
         return {
@@ -32,9 +51,9 @@ async function checkVoiceChannel(interaction, player) {
             .setAccentColor(0xff0000)
             .addTextDisplayComponents(
                 (textDisplay) => textDisplay.setContent(
-                    `${lang.utils.voiceChannelCheck.wrongChannel.title}\n\n` +
-                    `${lang.utils.voiceChannelCheck.wrongChannel.message.replace('{channelName}', channelName)}\n\n` +
-                    `${lang.utils.voiceChannelCheck.wrongChannel.note.replace('{channelName}', channelName)}`
+                    `${voiceCheck.wrongChannel.title}\n\n` +
+                    `${voiceCheck.wrongChannel.message.replace('{channelName}', channelName)}\n\n` +
+                    `${voiceCheck.wrongChannel.note.replace('{channelName}', channelName)}`
                 )
             );
         return {
